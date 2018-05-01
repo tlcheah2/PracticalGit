0c31382cb0b234ae876ee2e0562da060671ab380
Tek Loon <tlcheah2@users.noreply.github.com>
2018-05-01 21:28:28 +0800

    Add log to multiply method

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ function remainder(n1, n2) {
 }
 
 function multiply(n1, n2) {
-    return n1 * n2;
+    let result = n1 * n2;
+    console.log('n1 * n2 equal ', result);
+    return result;
 }
 
 function divide(n1, n2) {
